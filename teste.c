bff2564fb1e5a752bf341ed49ddf21ea048c5a32
Lucas Juan <lucasjuan14@gmail.com>
2018-11-05 14:24:17 -0200

    Bibliotecas e Define's

--- teste.c
+++ teste.c
@@ -1,1 +1,15 @@
-//TESTEE
+//bibliotecas importantes para o programa
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <conio.h>
+
+//Valores globais para usar no programa
+#define LINHAS 25
+#define COLUNAS 40
+#define VERDE 10
+#define AZUL 11
+#define AMARELO 14
+#define VERMELHO 12
+#define BRANCO 15
+#define CINZA 8
